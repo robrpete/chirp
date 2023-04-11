e3be76bf1d4c4b4360f8fa9776dafc821e053ffb
robrpete <rob.r.pete@gmail.com>
2023-04-11 16:48:54 -0400

    post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,8 +18,13 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton/>}
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton/>}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
